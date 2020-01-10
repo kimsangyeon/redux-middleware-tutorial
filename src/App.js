f2804b3e25789cf7de51c7a33ef195a35119cbce
sangyeon <dho113@naver.com>
2020-01-10 17:09:39 +0900

    thunk async axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import CounterContainer from './container/CounterContainer';
+import SampleContainer from './container/SampleContainer';
 
 const App = () => {
   return (
     <div>
-      <CounterContainer />
+      <SampleContainer />
     </div>
   );
 }
