d1d32afb9cdac772319394293354e3f68257f3d4
sangyeon <dho113@naver.com>
2020-01-16 16:11:53 +0900

    redux-saga effects select 사용

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import SampleContainer from './container/SampleContainer';
 const App = () => {
   return (
     <div>
+      <CounterContainer />
+      <hr />
       <SampleContainer />
     </div>
   );
